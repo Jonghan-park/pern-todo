ff45702cd3eff8dadee8e8ffde4af20c2bfdf409
Jonghan Park <jonghan.park@edu.sait.ca>
2023-01-16 16:28:29 -0700

    Set the database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+
+// create a todo
+
+// get all todos
+
+// get a todo
+
+// update a todo
+
+// delete a todo
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
