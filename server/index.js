9257614df89edfaaa77b78bf2517762974583b50
Jonghan Park <jonghan.park@edu.sait.ca>
2023-01-16 16:41:20 -0700

    Update get all todos

--- server/index.js
+++ server/index.js
@@ -10,8 +10,28 @@ app.use(express.json());
 // Routes
 
 // create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 // get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // get a todo
 
