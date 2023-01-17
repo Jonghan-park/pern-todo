757aaa11e56baf8f0036aef018641e8e8fde9278
Jonghan Park <jonghan.park@edu.sait.ca>
2023-01-17 15:44:23 -0700

    Get data from server and mapping

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 // components
 import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodos";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <InputTodo />
+      <ListTodos />
     </div>
   );
 }
--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
 
 const ListTodos = () => {
-  return <div>ListTodos</div>;
+  const [todos, setTodos] = useState([]);
+  const getTodos = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/todos");
+
+      setTodos(response.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  useEffect(() => {
+    getTodos();
+  }, []);
+  return (
+    <Fragment>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Description</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => {
+            return (
+              <tr>
+                <th scope="row">{todo.todo_id}</th>
+                <td>{todo.description}</td>
+                <td>Edit</td>
+                <td>Delete</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Fragment>
+  );
 };
 
 export default ListTodos;
