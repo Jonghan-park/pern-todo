f6c01989a9e0abdc7932aaa0a2e22931a256928f
Jonghan Park <jonghan.park@edu.sait.ca>
2023-01-18 08:24:10 -0700

    Deployed database

--- server/db.js
+++ server/db.js
@@ -1,10 +1,15 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
+
+const POSTGRESQL_PASSWORD = process.env.POSTGRESQL_PASSWORD;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
+  user: "jonghan",
+  host: "dpg-cf407m14rebfa0qme06g-a.oregon-postgres.render.com",
+  password: POSTGRESQL_PASSWORD,
   port: 5432,
   database: "perntodo",
+  ssl: "true",
 });
 
 module.exports = pool;
